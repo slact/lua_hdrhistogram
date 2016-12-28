3414a04f1a698f77dbc86ca992ed278920795a4b
Leo P <junk@slact.net>
2016-12-28 04:07:04 -0500

    yeah offsets and units

--- hdrhistogram.lua
+++ hdrhistogram.lua
@@ -1,12 +1,45 @@
 local hdr = require "hdrhistogram.hdr"
 
-local hdrmeta = getmetatable(hdr.new(1,1000,3))
+local hdrmeta = getmetatable(hdr.new(1, 1000, 2))
+
+local new = hdr.new
+
+local data = setmetatable({}, {__mode="k"})
+
+function hdr.new(lowest, highest, sig, opt)
+  
+  opt = opt or {}
+  local self = new(lowest, highest, sig)
+  print(type(self))
+  
+  data[self] = {
+    multiplier = opt.multiplier or 1,
+    unit = opt.unit or opt.units or ""
+  }
+  
+  local record = hdrmeta.record
+  function hdrmeta:record(val)
+    return record(self, val * 1/data[self].multiplier)
+  end
+  
+  return self
+end
+
+
+for i, v in ipairs({"min", "max", "mean", "stddev", "percentile"}) do
+  local orig = hdrmeta[v]
+  hdrmeta[v] = function(self, ...)
+    return orig(self, ...) * data[self].multiplier
+  end
+end
 
 function hdrmeta:stats(percentiles)
   percentiles = percentiles or {10,20,30,40,50,60,70,80,90,100}
   local out = {}
+  local pctf = data[self].multiplier < 1 and  "%12." .. math.ceil(math.abs(math.log10(data[self].multiplier, 10))) .. "f" or "%12u"
+  local fstr = "%7.3f%% "..pctf..data[self].unit
   for i,v in ipairs(percentiles) do
-    table.insert(out, ("#% 3d%%  % 6d"):format(v, self:percentile(v)))
+    table.insert(out, fstr:format(v, self:percentile(v)))
   end
   return table.concat(out, "\n")
 end
@@ -14,7 +47,7 @@ end
 function hdrmeta:latency_stats()
   local out = {
     "# Latency stats",
-    hdrhistogram.stats { 50, 75, 90, 95, 99, 99.9, 100 }
+    self:stats { 50, 75, 90, 95, 99, 99.9, 100 }
   }
   return table.concat(out, "\n")
 end
--- test.lua
+++ test.lua
@@ -1,27 +1,35 @@
 local hdrhistogram = require "hdrhistogram"
 require "string"
 
-local hdr = hdrhistogram.new(1,1000000,3)
+local multiplier = 0.001
+
+local hdr = hdrhistogram.new(1,1000000,3, {multiplier=multiplier, unit="ms"})
 
 local expected_min    = 0
 local expected_max    = 0
 local expected_p50    = 0
+
+for i,v in pairs(getmetatable(hdr)) do
+  print(i,v)
+end
 
 assert(hdr:min() == expected_min, string.format("incorrect min value expected: %d, received %d", expected_min, hdr:min()))
 assert(hdr:max() == expected_max, string.format("incorrect max value expected: %d, received %d", expected_max, hdr:max()))
 assert(hdr:percentile(50) == expected_p50, string.format("incorrect p50 value expected: %d, received %d", expected_p50, hdr:percentile(50)))
 
 for i = 10, 1000000, 10 do
-  hdr:record(i)
+  hdr:record(i*multiplier)
 end
 
-local expected_min    = 10
-local expected_max    = 1000447
-local expected_p50    = 500223
+local expected_min    = 10*multiplier
+local expected_max    = 1000447*multiplier
+local expected_p50    = 500223*multiplier
+print(hdr:min())
+assert(hdr:min() == expected_min, string.format("incorrect min value expected: %f, received %f", expected_min, hdr:min()))
+assert(hdr:max() == expected_max, string.format("incorrect max value expected: %f, received %f", expected_max, hdr:max()))
+assert(hdr:percentile(50) == expected_p50, string.format("incorrect p50 value expected: %f, received %f", expected_p50, hdr:percentile(50)))
 
-assert(hdr:min() == expected_min, string.format("incorrect min value expected: %d, received %d", expected_min, hdr:min()))
-assert(hdr:max() == expected_max, string.format("incorrect max value expected: %d, received %d", expected_max, hdr:max()))
-assert(hdr:percentile(50) == expected_p50, string.format("incorrect p50 value expected: %d, received %d", expected_p50, hdr:percentile(50)))
-
+print(hdr:stats())
+print(hdr:latency_stats())
 
 print("OK")
